64a95611e8cd94751cd06a3e55000bf8a981cff1
KalyaniMahajan <kalyanimahajan2014@gmail.com>
2024-12-08 14:45:44 +0530

    created routes and links in react js - UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Nav from './Nav.js';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        <h1>      E-Dashboard
-        </h1>
+      <BrowserRouter>
+        <Nav /> 
+        <Routes>
+          <Route path='/' element={<h1>Product Compo</h1>}></Route>
+          <Route path='/add' element={<h1>Add Product Compo</h1>}></Route>
+          <Route path='/update' element={<h1>Update Product Compo</h1>}></Route>
+          <Route path='/Logout' element={<h1>Logout Compo</h1>}></Route>
+          <Route path='/Profile' element={<h1>Profile Compo</h1>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
